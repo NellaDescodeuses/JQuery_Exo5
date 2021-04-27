ddff3a9b30ed399c13cc6777f63c11634346289f
NellaDescodeuses <nellaruscade.pro@gmail.com>
2021-04-27 11:16:19 +0200

    Ajout de l'exo

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    //lorque je clique sur le button,
+    $('button').click(function () { 
+        //la div id="text_2" se vide.
+        $('#text_2').empty();
+    });
+});
